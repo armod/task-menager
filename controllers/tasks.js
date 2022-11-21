503f217b0d0eed12eec09c8ed956ea1aa69861b6
armod <armods@gmail.com>
2022-11-21 20:57:55 +0100

    db connect string

--- controllers/tasks.js
+++ controllers/tasks.js
@@ -1,5 +1,21 @@
 const getAllTask = (req, res) => {
-  res.send('all items')
+  res.send('get all task')
 }
 
-module.exports = { getAllTask }
+const createTask = (req, res) => {
+  res.json(req.body)
+}
+
+const getTask = (req, res) => {
+  res.json({ id: req.params.id })
+}
+
+const updateTask = (req, res) => {
+  res.send('Update task')
+}
+
+const deleteTask = (req, res) => {
+  res.send('Delete task')
+}
+
+module.exports = { getAllTask, createTask, getTask, updateTask, deleteTask }
