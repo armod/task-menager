f1c22a57f959e6a96115da9f95d4f9ac6d726f1e
armod <armods@gmail.com>
2022-11-21 18:28:44 +0100

    routes

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log('Task Manager App')
+const express = require('express')
+const app = express()
+
+// routes
+app.get('/', (req, res) => {
+  res.send('Task Menager App')
+  // console.log('Task menager App')
+})
+
+const port = 3000
+
+app.listen(port, console.log(`server is listening at port ${port}..`))
