0d5a0f7d1fd9ac6e947ea4c2e62a1128abe8c74d
armod <armods@gmail.com>
2022-11-22 19:41:55 +0100

    connect to db refactor

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-require('./db/connect')
 const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
 
 // middleware
 app.use(express.json())
@@ -17,4 +17,13 @@ app.use('/api/v1/tasks', tasks)
 
 const port = 3000
 
-app.listen(port, console.log(`server is listening at port ${port}..`))
+const start = async () => {
+  try {
+    await connectDB()
+    app.listen(port, console.log(`server is listening at port ${port}..`))
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
--- db/connect.js
+++ db/connect.js
@@ -3,12 +3,16 @@ const mongoose = require('mongoose')
 const connectionString =
   'mongodb+srv://armod:armod123@nodeexpressprojects.bpaevnm.mongodb.net/TASK-MANAGER?retryWrites=true&w=majority'
 
-mongoose
-  .connect(connectionString, {
+const connectDB = (url) => {
+  return mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: false,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('CONNECTED TO THE DB...'))
-  .catch((err) => console.log(err))
+}
+
+// .then(() => console.log('CONNECTED TO THE DB...'))
+// .catch((err) => console.log(err))
+
+module.exports = connectDB
