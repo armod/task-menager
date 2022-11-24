44173cba7e425b95d9a040c2c01119717c9b5af0
armod <armods@gmail.com>
2022-11-24 21:06:33 +0100

    basic validation

--- models/Task.js
+++ models/Task.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const TaskSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: [true, 'must provide name'],
+    required: [true, 'Must provide name'],
     trim: true,
     maxlength: [20, 'name can not be more than 20 character'],
   },
